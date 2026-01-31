aefa9ee9163f56d4c6930da0323279d94ef4de52
GabrieleMaffia <60008@centrocasnati.eu>
2026-01-31 18:28:37 +0100

    aggiunto FixedUpdate
    ho aggiunto la funzione FixedUpdate per gestire la fisica del gioco (per ora c'è dentro l'applicazione dei movimenti orizzontale e verticale del giocatore)

--- Rosita-La-Guardia-Di-Ventaris/Assets/Scripts/PlayerController.cs
+++ Rosita-La-Guardia-Di-Ventaris/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
 
     float camRotationX, camRotationY;
     float horizontalAxis, verticalAxis;
+    Vector3 calculatedStrafeVelocity, calculatedLinearVelocity;
     Rigidbody playerRB;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,15 +27,11 @@ public class PlayerController : MonoBehaviour
     {
         //calcolo del movimento sull'asse orizzontale (strafe, funzionante sia su M&K che su controller)
         horizontalAxis = Input.GetAxis("Horizontal") * movementSpeed;
-        Vector3 calculatedStrafeVelocity = transform.right * horizontalAxis;
+        calculatedStrafeVelocity = transform.right * horizontalAxis;
 
         //calcolo del movimento sull'asse verticale (movimento avanti-indietro, funzionante sia su M&K che su controller)
         verticalAxis = Input.GetAxis("Vertical") * movementSpeed;
-        Vector3 calculatedLinearVelocity = transform.forward * verticalAxis;
-
-        //applicazione dei due movimenti calcolati
-        Vector3 calculatedTotalVelocity = calculatedStrafeVelocity + calculatedLinearVelocity;
-        playerRB.linearVelocity = new Vector3(calculatedTotalVelocity.x, playerRB.linearVelocity.y, calculatedTotalVelocity.z);
+        calculatedLinearVelocity = transform.forward * verticalAxis;
 
         //calcolo e applicazione della rotazione della visuale in su e in giù (funzionante sia su M&K che su controller)
         camRotationX = Mathf.Clamp(Input.GetAxis("Mouse X") + Input.GetAxis("Horizontal Secondary"), -1, 1);
@@ -42,5 +39,12 @@ public class PlayerController : MonoBehaviour
 
         camRotationY = Mathf.Clamp(Input.GetAxis("Mouse Y") + Input.GetAxis("Vertical Secondary"), -1, 1);
         view.transform.Rotate((-camRotationY * camRotationSpeedY) * Time.deltaTime, 0, 0);
+    }
+
+    void FixedUpdate()
+    {
+        //applicazione dei due movimenti calcolati
+        Vector3 calculatedTotalVelocity = calculatedStrafeVelocity + calculatedLinearVelocity;
+        playerRB.linearVelocity = new Vector3(calculatedTotalVelocity.x, playerRB.linearVelocity.y, calculatedTotalVelocity.z);
     }
 }
