d0e5fba11dc0b6082b61fd0a158f654aadb9071f
GabrieleMaffia <60008@centrocasnati.eu>
2026-05-21 21:12:58 +0200

    cambiamenti QoL + salto
    riordinato lo script "RositaCharacterController" e aggiunta la funzione di salto
    
    aggiunta anche una meccanica per la quale non puoi muoverti dopo aver saltato

--- Rosita-La-Guardia-Di-Ventaris/Assets/Scripts/RositaCharacterController.cs
+++ Rosita-La-Guardia-Di-Ventaris/Assets/Scripts/RositaCharacterController.cs
@@ -1,22 +1,43 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RositaCharacterController : MonoBehaviour
 {
-    public Animator animator;
-    public Rigidbody Rosita;
-    public float MoveSpeed = 5f;
+    public Animator Animator;
+    public Rigidbody RositaRB;
     public Transform CameraTransform;
+    public float MoveSpeed = 5f;
     public float RotationSpeed = 10f;
+    public float GroundThreshold = 1f;
+    public float JumpForce = 1f;
 
     Vector3 forward;
     Vector3 right;
     Vector3 Direction;
 
     float speedMultiplier = 1f;
-    bool pugni;
+    bool isPunching, hasJumped, isInAir;
 
     void Update()
     {
+        //salva l'informazione del corrente stato di animazione nella variabile state
+        AnimatorStateInfo state = Animator.GetCurrentAnimatorStateInfo(0);
+
+        //assegna a isPunching true se siamo nell'animazione dei pugni
+        isPunching = state.IsName("Punch");
+
+        //animazione dei pugni
+        if (Input.GetMouseButtonDown(0) && !isPunching)
+        {
+            Animator.SetTrigger("Punch");
+        }
+
+        #region Movimento
+        //variabili di movimento asse X e Z
+        float Horizontal = 0f;
+        float Vertical = 0f;
+
+        //calcolo di forward e right
         forward = CameraTransform.forward;
         right = CameraTransform.right;
 
@@ -26,18 +47,7 @@ public class RositaCharacterController : MonoBehaviour
         forward.Normalize();
         right.Normalize();
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            animator.SetTrigger("punch");
-        }
-
-        AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
-
-        pugni = state.IsName("Punch");
-
-        float Horizontal = 0f;
-        float Vertical = 0f;
-
+        //assegna i vari valori a Vertical e Horizontal in base al tasto premuto
         if (Input.GetKey(KeyCode.W))
             Vertical = 1f;
 
@@ -50,45 +60,68 @@ public class RositaCharacterController : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
             Horizontal = 1f;
 
-        bool isMoving = Horizontal != 0 || Vertical != 0;
+        //calcolo di isMoving e isRunning
+        bool isMoving = Mathf.Abs(Horizontal) > 0.01f || Mathf.Abs(Vertical) > 0.01f;
         bool isRunning = isMoving && Input.GetKey(KeyCode.LeftShift);
 
-        if (pugni)
-        {
-            speedMultiplier = 0.2f;
-        }
-        else if (isRunning)
-        {
-            speedMultiplier = 1.5f;
-        }
-        else
-        {
-            speedMultiplier = 1f;
-        }
+        //metti un moltiplicatore per la velocità in base a cosa sta facendo il giocatore
+        speedMultiplier = isPunching ? 0 : isRunning ? 1.5f : 1;
 
-        animator.SetBool("SoftRun", isMoving && !isRunning);
-        animator.SetBool("SpeedRun", isRunning);
+        //metti le animazioni corrette in base alla modalità di movimento del giocatore
+        Animator.SetBool("SoftRun", isMoving && !isRunning);
+        Animator.SetBool("SpeedRun", isRunning);
 
+        //calcolo della direzione di movimento
         Direction = (forward * Vertical) + (right * Horizontal);
         Direction.Normalize();
 
+        //calcolo e applicazione della velocità di movimento
         Vector3 velocity = Direction * MoveSpeed * speedMultiplier;
-
-        Rosita.linearVelocity = new Vector3(
-            velocity.x,
-            Rosita.linearVelocity.y,
-            velocity.z
-        );
+        RositaRB.linearVelocity = new Vector3(velocity.x, RositaRB.linearVelocity.y, velocity.z);
 
         if (isMoving)
         {
             Quaternion targetRotation = Quaternion.LookRotation(Direction, Vector3.up);
 
-            transform.rotation = Quaternion.RotateTowards(
-                transform.rotation,
-                targetRotation,
-                RotationSpeed * Time.deltaTime
-            );
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
+        }
+        #endregion
+
+        //controllo del salto
+        if (isGrounded() && Input.GetButtonDown("Jump"))
+        {
+            hasJumped = true;
+            isInAir = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //fisica del salto
+        if (hasJumped)
+        {
+            RositaRB.AddForce(0, JumpForce, 0);
+            hasJumped = false;
+        }
+    }
+
+    bool isGrounded()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, out hit))
+        {
+            if (hit.distance > GroundThreshold)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else
+        {
+            return false;
         }
     }
 }
